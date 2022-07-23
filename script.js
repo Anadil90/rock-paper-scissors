7d20c1b437ab68c8c7c20795fe45419981e9b63d
Anadil90 <45528659+Anadil90@users.noreply.github.com>
2022-07-23 18:27:45 +0600

    initial commit

--- script.js
+++ script.js
@@ -2,126 +2,111 @@ const item = ["rock", "paper", "scissors"] //player item array
 
 let computerSelection = computerPlay() //computer randomly selects object
 
-// DOM elements 
-const player1display = document.getElementById("player1Display"); //player 1 selection display
+// player buttons ............................>
+const p1Buttons = document.querySelectorAll(".p1Buttons");
+const p2Buttons = document.querySelectorAll(".p2Buttons");
+// .......................................................>
+
+// UI elements ...................................................>
+const player1Display = document.getElementById("player1Display"); //player 1 selection display
 const player2Display = document.getElementById("player2Display"); //player 2 selection display
-const buttons = document.getElementsByTagName("button"); // div containing player buttons
-const switchPlayer = document.getElementById("switchPlayer") //toggle between player 2 and computer
-const secondPlayer = document.querySelectorAll(".playerName")[1]
+const switchPlayer = document.getElementById("switchPlayer"); //toggle between player 2 and computer
+let score1 = document.getElementById("score1"); //player 1 score 
+let score2 = document.getElementById("score2"); //player 2 score
+// .................................................................>
+const selection = ["Player 2", "Computer"] //selection array
 
-
-function p1Select(item) {
-    player1display.innerText = item
+function selectPlayer() { //chooses who to play with (player2 or computer)
+    
+    let count = 0; 
+    let comp;
+    switchPlayer.addEventListener("click", () => {
+        count ++; //increment count 
+        for(i = 0; i < 2; i++) {
+            if(count < 2) {
+                switchPlayer.innerText = selection[count]; //toggle innerText when user clicks button
+                
+            }
+            else{
+                count = 0 //reset counter to 0
+                switchPlayer.innerText = selection[count]; //toggle innerText when user clicks button
+                
+            }
+        }
+        
+    })
 }
-
-function p2Select(item) {
-    player2Display.innerText = item
-}
+selectPlayer()
 
 function computerPlay () {
-    let randomIndex = Math.random(Math.floor() * item.length) //returns a random index of array from 0 to 2
+    let randomIndex = Math.floor(Math.random() * item.length) //returns a random index of array from 0 to 2
     return item[randomIndex] //random item selection from array
 }
 
-function selectPlayer() {
-    let player2 = false;
-    let x = false;
-    let switchCounter = ["Player 1", "Computer"]
-        let counter = []
-        let count = 0;
-        switchPlayer.addEventListener("click", () => {
-            
-            count ++; //increment count 
-            for(i = 0; i < 2; i++) {
-                if(count < 2) {
-                secondPlayer.innerText = switchCounter[count] //toggle innerText when user clicks button
-                
-                }
-                else{
-                    
-                    count = 0
-                    secondPlayer.innerText = switchCounter[count] //toggle innerText when user clicks button
-                }
-            }
+let p1Selects; //selection of player 1
+let p2Selects; //selection of player 2
+
+function player1Selection() {
+    for(let i = 0; i < item.length; i++) {
+        p1Buttons[i].addEventListener("click", () => {
+        player1Display.value = item[i]
+        p1Selects = player1Display.value
         
+        if(p1Selects === "rock" && p2Selects === "scissors") {
+            console.log("p1 won")
+            score1.innerText ++
+            
+        }
+
+        
+
+        if(p1Selects === "scissors" && p2Selects === "rock") {
+            console.log("p2 won")
+            score2.innerText ++
+        }
+
+        if(p1Selects === "rock" && p2Selects === "paper") {
+            console.log("p2 won")
+            score1.innerText ++
+        }
+
+        if(p1Selects === "scissors" && p2Selects === "paper") {
+            console.log("p1 won")
+            score1.innerText ++
+        }
+
+
+        })
+        
+    }
+
+}
+player1Selection()
+
+function player2Selection() {
+    for(let i = 0; i < item.length; i++) {
+        p2Buttons[i].addEventListener("click", () => {
+        player2Display.value = item[i]
+        p2Selects = player2Display.value //set selection items of player 2
+
+        if(p2Selects === "rock" && p1Selects === "scissors") {
+            console.log("p2 won")
+            score2.innerText ++
+        }
+
+        if(p2Selects === "paper" && p1Selects === "rock") {
+            console.log("p2 won")
+            score2.innerText ++
+        }
+
+        if(p2Selects === "scissors" && p1Selects === "paper") {
+            console.log("p2 won")
+            score2.innerText ++
+        }
+    
         })
     }
-    
-selectPlayer()
 
-const playerWin = "You win!";
-const computerWin = "Computer wins!";
-let roundWinner = "";
-
-function playGame(playerSelection, computerSelection) {
-    
-    if(playerSelection === "rock" && computerSelection === "scissors") {
-        displayValue = "rock"
-        alert("yoiu won")
-    }
-
-    else if (computerSelection === "rock" && playerSelection === "scissors") {
-        console.log(`Computer wins ${computerWin}`)
-        roundWinner = "computer"
-    }
-
-    if(playerSelection === "paper" && computerSelection === "rock") {
-        
-        console.log(`Paper covers rock. ${playerWin}`)
-        roundWinner = "player"
-    }
-
-    else if (computerSelection === "paper" && playerSelection === "rock") {
-        console.log(`${computerWin}`)
-        roundWinner = "computer"
-    }
-
-    if(playerSelection === "scissors" && computerSelection === "paper") {
-        
-        console.log(`You win! Scissors cut paper ${playerWin}`)
-        roundWinner = "player"
-    }
-
-    else if (computerSelection === "scissors" && playerSelection === "paper") {
-        console.log("Computer wins")
-        roundWinner = "computer"
-        
-    }
-    
-    else if (computerSelection === playerSelection){
-        console.log("Its a tie!")
-        
-    }
-   
 }
+player2Selection()
 
-let player = 0;
-let computer = 0;
-
-function playRound () {
-    for(let i = 0; i < 5; i++) {
-
-        playGame()
-
-        if(roundWinner == "player") {
-             player ++
-        }
-
-        if(roundWinner == "computer") {
-            computer ++
-        }
-        
-    }
-    
-    if(player > computer) {
-        console.log("The player beat the computer!")
-        console.log(`Player score is ${player} Computer score is ${computer}`)
-    }
-    else {
-        console.log("The computer beat the player!")
-        console.log(`Player score is ${player} Computer score is ${computer}`)
-    }  
-    
-}
-
-playRound()
